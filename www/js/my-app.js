7453a95e441c40e0b0263438382faa7fc6072e5e
Bynnes <falecom@bynn.es>
2020-01-14 11:51:43 -0300

    update

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -13523,7 +13523,7 @@ function limpar()
                 },
                 ios: {
                     senderID: "230054065669",
-                    gcmSandbox: "true", // false para producao true para desenvolvimento
+                    gcmSandbox: "false", // false para producao true para desenvolvimento
                     alert: "true",
                     sound: "true",
                     badge: "false"
